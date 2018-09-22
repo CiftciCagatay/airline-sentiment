a78b4d8df3a9f2edbdbbdf5546793c27f9c67795
Cagatay Ciftci <im.cgtycftc@gmail.com>
2018-09-22 18:01:57 +0300

    .

--- model.py
+++ model.py
@@ -13,26 +13,27 @@ from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
 from nltk.corpus import stopwords
 from collections import Counter
-from autocorrect import spell
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import SGDClassifier
-from sklearn.naive_bayes import MultinomialNB
 from sklearn.cross_validation import train_test_split
+import pickle
 
 # Import Dataset
 dataset = pd.read_csv('Airline-Sentiment.csv')
 X = dataset.iloc[:, 1].values
 y = dataset.iloc[:, 0].values
-
+# 8 - 10 kasım
 # Vars.
 stemmer = PorterStemmer()
 corpus = []
-# Regex string to remove hashtags, punc. etc.
-regex = '(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)'
+
 # English dictionary file for splitting words
 dictionary = Counter(words(open('words.txt').read()))
 max_word_length = max(map(len, dictionary))
 total = float(sum(dictionary.values()))
+
+# Regex string to remove hashtags, punc. etc.
+regex = '([@&][A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)'
 
 # Process all the comments in dataset
 for i in range(0, len(X)):
@@ -40,15 +41,16 @@ for i in range(0, len(X)):
     print(i)
 
 # Vectorize each comment by freq. 'customer' 'customer service'
-tfidf_vect = TfidfVectorizer(ngram_range=(1,2))
+tfidf_vect = TfidfVectorizer(ngram_range=(1,2), max_features=5000)
 X_tfidf = tfidf_vect.fit_transform(raw_documents=X).toarray()
 
 # Split test and train datasets
 X_train, X_test, y_train, y_test = train_test_split(X_tfidf, y, test_size=0.2)
 
 # Create and train classifier
-clf = MultinomialNB()
-clf = clf.fit(X_train, y_train)
+# Create and train classifier
+clf = SGDClassifier()
+clf.fit(X_train, y_train)
 
 # Predict 
 y_pred = clf.predict(X_test)
@@ -56,32 +58,16 @@ y_pred = clf.predict(X_test)
 # Calculate accuracy
 accuracy = np.mean(y_pred == y_test)
 
-features = tfidf_vect.get_feature_names()
-df = pd.DataFrame(X_tfidf, columns=features)
+def dump_model():
+    filename = 'finalized_model.sav'
+    pickle.dump(clf, open(filename, 'wb'))
 
-highest_frequency = df.max()
+def predict(comment):
+    return clf.predict(tfidf_vect.transform(raw_documents=[process_comment(comment)]).toarray())
 
-def process_comment(comment):
-    # Remove punc. hashtags etc.
-    comment = re.sub(regex, ' ', comment)
+def word_prob(word): return dictionary[word] / total
 
-    comment = comment.lower()
-    comment = word_tokenize(comment)
-    
-    # Split words
-    new = []
-    for i in range(len(comment)):
-        for word in viterbi_segment(comment[i])[0]:
-            new.append(word)
-            
-    comment = new
-    
-    # Remove stopwords and stem, spellcheck every word in comments
-    comment = [stemmer.stem(spell(word)) for word in comment if not word in stopwords.words('english')]
-    
-    # Normalize comment
-    comment = ' '.join(comment)
-    return comment
+def words(text): return re.findall('[a-z]+', text.lower()) 
 
 def viterbi_segment(text):
     probs, lasts = [1.0], [0]
@@ -98,9 +84,22 @@ def viterbi_segment(text):
     words.reverse()
     return words, probs[-1]
 
-def word_prob(word): return dictionary[word] / total
+def process_comment(comment):
+    # Remove punc. hashtags etc.
+    comment = re.sub(regex, ' ', comment)
 
-def words(text): return re.findall('[a-z]+', text.lower()) 
+    comment = comment.lower()
+    comment = word_tokenize(comment)
 
+    new = []
+    for i in range(len(comment)):
+        for word in viterbi_segment(comment[i])[0]:
+            new.append(word)
+    comment = new    
 
-
+    # Remove stopwords and stem, spellcheck every word in comments
+    comment = [stemmer.stem(word) for word in comment if not word in stopwords.words('english')]
+    
+    # Normalize comment
+    comment = ' '.join(comment)
+    return comment
